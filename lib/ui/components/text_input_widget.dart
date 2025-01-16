1ff9bc667822efa0a8133c91c5664c730c1881b2
kikuchi <mayo.ketya.aurore@gmail.com>
2025-01-17 02:07:29 +0900

    テキスト入力フォームウィジェットでバリデーションが行われるように修正

--- lib/ui/components/text_input_widget.dart
+++ lib/ui/components/text_input_widget.dart
@@ -1,29 +1,41 @@
 import 'package:flutter/material.dart';
 
+import '../../domain/validators/validator.dart';
+
 /// テキスト入力フォームウィジェット
 class TextInputWidget extends StatelessWidget {
   const TextInputWidget({
     super.key,
     required this.controller,
     required this.hintText,
+    required this.validator,
   });
 
   /// コントローラー
   final TextEditingController controller;
+
   /// ヒントテキスト
   final String hintText;
+
+  /// バリデータ
+  final Validator validator;
 
   @override
   Widget build(BuildContext context) {
     return Column(children: [
-      TextField(
+      TextFormField(
         controller: controller,
         decoration: InputDecoration(
-          border: InputBorder.none,
-          isDense: true,
-          contentPadding: EdgeInsets.zero,
-          hintText: hintText,
-        ),
+            border: InputBorder.none, isDense: true, contentPadding: EdgeInsets.zero, hintText: hintText, counterText: ''),
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        validator: (String? value) {
+          final result = validator.validate(controller.text);
+          return result.when(
+            success: () => null,
+            error: (message) => message,
+          );
+        },
+        maxLength: 100,
       ),
       Divider(),
     ]);
