86083d4e230e64a8401428be9c9cf6a97608e430
kikuchi <mayo.ketya.aurore@gmail.com>
2025-01-17 23:47:48 +0900

    TitleValidatorのProviderを実装

---
+++ lib/domain/validators/title_validator.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'title_validator.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$titleValidatorHash() => r'0aceab4555da94a51eb3beacfc30e104c9328989';
+
+/// See also [titleValidator].
+@ProviderFor(titleValidator)
+final titleValidatorProvider = AutoDisposeProvider<TitleValidator>.internal(
+  titleValidator,
+  name: r'titleValidatorProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$titleValidatorHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef TitleValidatorRef = AutoDisposeProviderRef<TitleValidator>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
