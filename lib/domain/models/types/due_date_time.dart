e93a6aea7c29c100cb8c4350dd8fa7e0e634c93c
kikuchi <mayo.ketya.aurore@gmail.com>
2025-01-19 22:15:44 +0900

    DueDateTimeのISO8601文字列変換関数を実装

--- lib/domain/models/types/due_date_time.dart
+++ lib/domain/models/types/due_date_time.dart
@@ -4,4 +4,14 @@ extension type DueDateTime(DateTime dateTime) {
   String formattedString() {
     return "${dateTime.year}-${dateTime.month.toString().padLeft(2, '0')}-${dateTime.day.toString().padLeft(2, '0')}";
   }
+
+  /// 締切日時のISO8601文字列を返す
+  toIso8601String() {
+    return dateTime.toIso8601String();
+  }
+
+  /// ISO8601文字列から締切日時を生成する
+  static fromIso8601String(map) {
+    return DueDateTime(DateTime.parse(map));
+  }
 }
